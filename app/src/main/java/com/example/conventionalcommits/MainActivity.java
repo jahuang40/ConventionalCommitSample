f2a61f4eb5f2f454fe468c6665b3440174f066de
jahuang40 <jahuang@pdftron.com>
2022-03-09 13:55:22 -0800

    xodoactionfeature: testing xodo action feature commit style

--- app/src/main/java/com/example/conventionalcommits/MainActivity.java
+++ app/src/main/java/com/example/conventionalcommits/MainActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Comment
-    //Comment
+    //XodoFeatureTestComment
+    //XodoActionFeatureTestComment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
