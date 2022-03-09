67b5fc281e7226e263cb1c008b0a937494ee285d
jahuang40 <jahuang@pdftron.com>
2022-03-09 13:57:09 -0800

    xodofix: testing xodo fix commit style

--- app/src/main/java/com/example/conventionalcommits/MainActivity.java
+++ app/src/main/java/com/example/conventionalcommits/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     //XodoFeatureTestComment
     //XodoActionFeatureTestComment
     //SupportTestComment
+    //XodoFixTestComment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
