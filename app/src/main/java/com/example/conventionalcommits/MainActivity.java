dca01ac9a31e12bfddd5d5212e2cddca6efd955c
jahuang40 <jahuang@pdftron.com>
2022-03-09 14:00:26 -0800

    analytics: testing analytics commit style

--- app/src/main/java/com/example/conventionalcommits/MainActivity.java
+++ app/src/main/java/com/example/conventionalcommits/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     //SupportTestComment
     //XodoFixTestComment
     //DevTestComment
+    //AnalyticsTestComment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
